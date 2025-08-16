abbfd35f8c6e08512bcb81e433bb93a885a5d1a9
暇な人 <yuuta4083@gmail.com>
2025-08-16 15:30:04 +0900

    ヘルプの更新など

--- modules/events/handle_event.js
+++ modules/events/handle_event.js
@@ -1,5 +1,9 @@
+// メッセージ受信時
 import command from "./message/command.js";
+
+// メンバー参加時
 import ban from "./join_member/ban.js";
+import welcome from "./join_member/welcome.js";
 
 import * as LINETypes from "@evex/linejs-types";
 
@@ -13,5 +17,6 @@ export default async function handle_event(client, op, commands) {
         command(client, message, commands);
 	} else if (op.type === "NOTIFIED_JOIN_CHAT") {
         await ban(client, op);
+        await welcome(client, op);
     }
 }
